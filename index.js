5554645a446d017ef573d72aae9c9a0b70910d72
golove <liuziyu7@gmail.com>
2022-01-16 14:23:03 +0800

    rebuild

--- index.js
+++ index.js
@@ -1,23 +1,85 @@
-const { getPage, findHref, findImgsrc } = require('./model');
+const request = require("request");
+// const cheerio = require('cheerio');
 
-let url = 'https://k6.c5cbca7s.pw/pw/thread.php?fid=14';
-
-
-
-async function main() {
-    let alldata = []
-    let data = await getPage(url);
-    // console.log(data)
-    let hrefs = findHref(data.res)
-    // console.log(hrefs)
-    for (const e of hrefs) {
-        let html = await getPage(e)
-        let srcs = findImgsrc(html.res)
-        alldata.unshift(srcs)
-    }
-
-    console.log(alldata, alldata.length)
-
+const promiseRequest = (url) => {
+  const userAgents = [
+    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0) ,Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0b13pre) Gecko/20110307 Firefox/4.0b13pre",
+    "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)",
+    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+    "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)",
+    "Opera/9.25 (Windows NT 5.1; U; en), Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+  ];
+  const userAgent = userAgents[Math.floor(Math.random() * userAgents.length)];
+  const headers = { "User-Agent": userAgent };
+  const opts = { url, headers };
+  return new Promise((resolve,reject) => {
+    request(opts, function (error, response, body) {
+      if (response.statusCode === 200) {
+        resolve(body);
+      }else{
+        reject(error)
+      }
+    });
+  });
+};
+ function getHref(html) {
+  const b = /html_data\/14\/([^<>"]*)/gi;
+  const href = html.match(b);
+  return Array.from(new Set(href));
 }
 
-main()
+ function getSrc(html) {
+  const b = /http:\/\/p1.pi22y.cc\/([^<>"]*)\.jpg/gi;
+  const srcs = html.match(b);
+  // 匹配 img 的 title 值
+  const t = /<span id="subject_tpc">(.*?)<\/span>/gi;
+  const title = t.exec(html);
+  const img = {
+    title: title[1],
+    href: url,
+    srcs: Array.from(new Set(srcs)),
+    star: 0,
+    collect: false,
+    deleted: false,
+    download: false,
+  };
+  // console.log(img)
+  return img;
+}
+
+const url = "http://w11.a6def2ef910.rocks/pw/thread.php?fid=14&page=3";
+const host = "http://w11.a6def2ef910.rocks/pw/";
+
+const imgArr = []
+async function doSpider(url){
+  const html = await promiseRequest(url)
+    if(url ==='http://w11.a6def2ef910.rocks/pw/thread.php?fid=14&page=3'){
+      const hrefs = getHref(html)
+      for (const iterator of hrefs) {
+        doSpider(host + iterator).catch(err=>{
+          console.error(err)
+        })
+      }
+    }else{
+      if(html) {
+        imgArr.push(getSrc(html))
+      }   
+    }
+}
+doSpider(url).catch(err=>{
+  console.error('first err '+err)
+})
